4425a7109c21f2eb2e0614a915d1e533546a6a0f
Craig <suesscraig112715@gmail.com>
2021-03-17 14:50:55 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  greeting = [hi, hello, hiya, goodbye]
+  ["hi", "hello", "hiya", "goodbye"];
 end
 
 def add_element_to_end_of_array(array, element)
